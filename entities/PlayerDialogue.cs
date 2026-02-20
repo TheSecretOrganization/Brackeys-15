b6d734f6608c223838366a86350d7d23416d796d
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2026-02-20 14:26:25 +0100

    format

--- entities/PlayerDialogue.cs
+++ entities/PlayerDialogue.cs
@@ -4,36 +4,36 @@ using System;
 public partial class PlayerDialogue : CenterContainer
 {
 
-	[Export] private Label _label;
-	[Export] private Timer _timer;
+    [Export] private Label _label;
+    [Export] private Timer _timer;
 
-	private int _index;
-	private string[] _textQueue;
+    private int _index;
+    private string[] _textQueue;
 
-	public override void _Ready()
-	{
-		_timer.Timeout += ClearText;
-	}
+    public override void _Ready()
+    {
+        _timer.Timeout += ClearText;
+    }
 
-	public void Show(string[] texts)
-	{
-		_index = 0;
-		_textQueue = texts;
-		IterateText();
-	}
+    public void Show(string[] texts)
+    {
+        _index = 0;
+        _textQueue = texts;
+        IterateText();
+    }
 
-	private void IterateText()
-	{
-		if (_index >= _textQueue.Length) return;
-		_label.Text = _textQueue[_index];
-		_index += 1;
-		_timer.Start();
-	}
+    private void IterateText()
+    {
+        if (_index >= _textQueue.Length) return;
+        _label.Text = _textQueue[_index];
+        _index += 1;
+        _timer.Start();
+    }
 
-	private void ClearText()
-	{
-		_label.Text = "";
-		IterateText();
-	}
-	
+    private void ClearText()
+    {
+        _label.Text = "";
+        IterateText();
+    }
+
 }
--- scripts/Dialogue.cs
+++ scripts/Dialogue.cs
@@ -3,18 +3,18 @@ using Godot;
 public partial class Dialogue : Area2D
 {
 
-	[Export] private string[] _texts;
+    [Export] private string[] _texts;
 
-	public override void _Ready()
-	{
-		BodyEntered += OnBodyEntered;
-	}
+    public override void _Ready()
+    {
+        BodyEntered += OnBodyEntered;
+    }
 
-	private void OnBodyEntered(Node2D body)
-	{
-		if (body is not Player player) return;
-		QueueFree();
-		player.Dialogue.Show(_texts);
-	}
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is not Player player) return;
+        QueueFree();
+        player.Dialogue.Show(_texts);
+    }
 
 }
